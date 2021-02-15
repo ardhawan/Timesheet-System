bd1d9acd2994e6c2fb7e8975b5b502cecdc366f0
arnavdhawan <47376316+arnavdhawan@users.noreply.github.com>
2021-02-15 08:59:38 +0800

    Added link for  backend student message method

--- server/api/user/route/user.js
+++ server/api/user/route/user.js
@@ -3,11 +3,13 @@ const router = express.Router();
 // const auth = require("../../../config/auth");
 const userController = require("../controller/userController");
 const deadlineController = require("../controller/deadlineController");
+const emailStudentController = require("../controller/emailStudentController");
 
 router.post("/login", userController.loginUser);
 router.post("/register", userController.registerNewUser);
 router.get("/getstudentd", deadlineController.getStudentDeadline);
 router.get("/getstaffd", deadlineController.getStaffDeadline);
 router.post("/setd", deadlineController.setDeadline);
+router.post("/sendstudentm", emailStudentController.sendStudentEmail);
 
 module.exports = router;
