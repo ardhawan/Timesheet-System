9d117735dad1e64e0e610eb36758be2a2f52dfa9
arnavdhawan <47376316+arnavdhawan@users.noreply.github.com>
2021-02-16 04:03:07 +0800

    Added link for  backend staff message method

--- server/api/user/route/user.js
+++ server/api/user/route/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userController = require("../controller/userController");
 const deadlineController = require("../controller/deadlineController");
 const emailStudentController = require("../controller/emailStudentController");
+const emailStaffController = require("../controller/emailStaffController");
 
 router.post("/login", userController.loginUser);
 router.post("/register", userController.registerNewUser);
@@ -11,5 +12,6 @@ router.get("/getstudentd", deadlineController.getStudentDeadline);
 router.get("/getstaffd", deadlineController.getStaffDeadline);
 router.post("/setd", deadlineController.setDeadline);
 router.post("/sendstudentm", emailStudentController.sendStudentEmail);
+router.post("/sendstaffm", emailStaffController.sendStaffEmail);
 
 module.exports = router;
